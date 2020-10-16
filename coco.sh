261e7ec12d64321f5bc9a4fd00e55161e20441f9
Aditya Sarma <aditya@wadhwaniai.org>
2020-10-17 03:37:38 +0530

    Update run scripts coco and halpe with cmd line args

--- coco.sh
+++ coco.sh
@@ -1,5 +1,7 @@
+indir=$1
+
 python scripts/demo_inference.py --cfg configs/coco/resnet/256x192_res50_lr1e-3_1x-duc.yaml \
             --checkpoint pretrained_models/fast_421_res50-shuffle_256x192.pth \
-            --indir /scratch/data/readonly/real_data/unpaired/raw/anthrofieldmaster/gujarat/home/jhagadia/863794047816719_11032020105238/video_1_baby_chessboard_ruler_8537765562257141228/ \
-            --outdir .
+            --indir $indir \
+            --outdir $indir
 
--- halpe.sh
+++ halpe.sh
@@ -1,4 +1,6 @@
+indir=$1
+
 python scripts/demo_inference.py --cfg configs/halpe_26/resnet/256x192_res50_lr1e-3_1x.yaml \
             --checkpoint pretrained_models/halpe26_fast_res50_256x192.pth \
-            --indir /scratch/data/readonly/real_data/unpaired/raw/anthrofieldmaster/gujarat/home/jhagadia/863794047816719_11032020105238/video_1_baby_chessboard_ruler_8537765562257141228/ \
-            --outdir .
+            --indir $indir \
+            --outdir $indir
